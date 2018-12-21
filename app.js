349d885c8aadaa419d221028460ad7545457ad6d
Sam <sam@samhooker.com>
2018-12-21 14:22:14 -0800

    updated

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+
+
+for(i=0;i<document.querySelectorAll(".note").length;i++){
+    document.querySelectorAll(".note")[i].addEventListener('click', function (){
+        
+})
+}
+
+
 let do1 = document.querySelector('.do')
 let re1 = document.querySelector('.re')
 let mi1 = document.querySelector('.mi')
@@ -21,6 +30,8 @@ function playDo(){
     do1.classList.add('animated');
     do1.classList.add('jello');
     setTimeout(doRemove, 2000);
+    const audio1 = new Audio('/notes/do1.wav');
+    audio1.play();
 }
 
 function doRemove() {
@@ -29,10 +40,12 @@ function doRemove() {
 }
 
 function playRe(){
-    document.body.style.background = 'black'
+    document.body.style.background = 'DarkGrey'
     re1.classList.add('animated');
     re1.classList.add('rubberBand');
     setTimeout(reRemove, 2000);
+    const audio2 = new Audio('/notes/re.wav');
+    audio2.play();
 }
 
 function reRemove() {
@@ -45,6 +58,8 @@ function playMi(){
     mi1.classList.add('animated');
     mi1.classList.add('wobble');
     setTimeout(miRemove, 2000);
+    const audio3 = new Audio('/notes/mi.wav');
+    audio3.play();
 }
 
 function miRemove() {
@@ -54,21 +69,73 @@ function miRemove() {
 
 function playFa(){
     document.body.style.background = 'brown'
+    fa1.classList.add('animated');
+    fa1.classList.add('flipInX');
+    setTimeout(faRemove, 2000);
+    const audio3 = new Audio('/notes/fa.wav');
+    audio3.play();
+}
+
+function faRemove() {
+    fa1.classList.remove("animated");
+    fa1.classList.remove('flipInX');
 }
 
 function playSo(){
     document.body.style.background = 'lightpink'
+    so1.classList.add('animated');
+    so1.classList.add('tada');
+    setTimeout(soRemove, 2000);
+    const audio3 = new Audio('/notes/so.wav');
+    audio3.play();
+}
+
+function soRemove() {
+    so1.classList.remove("animated");
+    so1.classList.remove('tada');
 }
 
 function playLa(){
-    document.body.style.background = 'yellow'
+    document.body.style.background = 'LightYellow'
+    la1.classList.add('animated');
+    la1.classList.add('flash');
+    setTimeout(laRemove, 2000);
+    const audio3 = new Audio('/notes/la.wav');
+    audio3.play();
 }
+
+function laRemove() {
+    la1.classList.remove("animated");
+    la1.classList.remove('flash');
+}
+
 
 function playTi(){
     document.body.style.background = 'lime'
+    ti1.classList.add('animated');
+    ti1.classList.add('headShake');
+    setTimeout(tiRemove, 2000);
+    const audio3 = new Audio('/notes/ti.wav');
+    audio3.play();
+}
+
+function tiRemove() {
+    ti1.classList.remove("animated");
+    ti1.classList.remove('headShake');
 }
 
 function playDo2(){
-    console.log('re works too')
+    document.body.style.background = 'skyblue'
+    do2.classList.add('animated');
+    do2.classList.add('heartBeat');
+    setTimeout(do2Remove, 2000);
+    const audio3 = new Audio('/notes/do2.wav');
+    audio3.play();
 }
+
+function do2Remove() {
+    do2.classList.remove("animated");
+    do2.classList.remove('heartBeat');
+}
+
 
